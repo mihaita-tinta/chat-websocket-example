16a1bd303b63588af0f4cc36b42d6b0bc3f9b61b
mihaita.tinta <mihaita.tinta@yahoo.com>
2016-03-16 17:20:06 +0200

    apk

--- chat-server/src/main/java/com/mihaita/chat/controller/HomeController.java
+++ chat-server/src/main/java/com/mihaita/chat/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.mihaita.chat.controller;
 
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -9,5 +15,11 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home() {
         return "redirect:/index.html";
+	}
+	
+
+	@RequestMapping(value="/android-debug.apk", produces={MediaType.APPLICATION_OCTET_STREAM_VALUE})
+	public @ResponseBody FileSystemResource apk() throws IOException {
+        return new FileSystemResource(new ClassPathResource("android-debug.apk").getFile());
 	}
 }
